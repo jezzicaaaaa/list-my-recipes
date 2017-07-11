db8f9d8e9691f25ef681436255b8025b34baa3e3
Jezzica <jezzicamae@gmail.com>
2017-07-11 08:04:47 -0400

    js basic knowledge with lists of recipe results/ functioning modal with displayed ingredients/back-pages function

--- js/script.js
+++ js/script.js
@@ -5,19 +5,30 @@ $('.recipe-lists').hide();
 $('.btn-home').on("click" , function(){
     $('.home').hide();
     $('.search').fadeIn();
-//   $("#ingredients").on("focus", function() {
-//         $("#ingredients").val("");
+    $('#go-home').on("click",function(){
+        $('.home').show();
+        $('.search').hide();
+    });
+    $('#go-search').on("click",function(){
+        $('.search').show();
+        $('.recipe-lists').hide();
+    });
+
 });
 
 var recipes = [];
+var ingredientsList = [];
     //Make inout values be gone on focus
-    $(function() {
-        var input = $('input[type=text]');
-        input.focus(function() {
-            $(this).val('');
-        });
-    });
+        var input = $('input.ingredients');
 
+
+    function addIngredientsToList(index){
+        $('input.ingredients').each(function(index){
+            // console.log($(this).val());
+            ingredientsList.push($(this).val())
+
+        });
+    }
 
     //Add and remove input fields
 
@@ -27,7 +38,7 @@ var recipes = [];
             e.preventDefault();
             if(x < maxItems){
                 x++;
-            $(".add-input").append('<div><input id="ingredients" class="ingredients" type="text"><i id="minus" class="fa fa-minus-circle" aria-hidden="true"></i><div>');
+            $(".add-input").append('<div><input class="ingredients" type="text"><i id="minus" class="fa fa-minus-circle" aria-hidden="true"></i><div>');
             }
             
             });
@@ -40,6 +51,7 @@ var recipes = [];
   
     $("form").on("submit", function(e){
         e.preventDefault();
+        addIngredientsToList();
         $('.search').hide();
         $('.recipe-lists').fadeIn();
         var ingredients = $('.ingredients').val();
@@ -49,7 +61,7 @@ var recipes = [];
             dataType: "json",
             data: {
                 key: "882f2168d0e0186c5f6ffdbd0e93b5e1",
-                q: ingredients
+                q: ingredientsList.join()
             },
         })
         .done(function(data){
@@ -59,26 +71,58 @@ var recipes = [];
 
     });
 
-   
 
-
-    //Create a loop to run results and put it in an array
+    //Loop through recipeList array and append results to html
     function listMyRecipe(recipeList){
         for ( i= 0; i < recipeList.length; i++ ){
-            var mylists = recipeList[i];  
-
-        //Make a template to append results to//
-        var template = `
-        <div class = "lists row col-sm-6 col-lg-6 col-md-6">
-            <h3> ${mylists.title}</h3>
-            <a href = "${mylists.f2f_url}" target="_blank">
-                <img src =" ${mylists.image_url} ">
-            </a>
-        </div> 
-        `;            
-        console.log(mylists);   
-        $('.results').append(template);
+            var item = recipeList[i];  
+            //Make a template to append to results
+            var template = `
+            <div class="item col-sm-6 col-lg-6 col-md-6" data-id='${item.recipe_id}'>
+                <img class="imageModal" src="${item.image_url}">
+                <h3>${item.title}</h3>
+            </div> 
+            `;            
+            // console.log($('.item').data("id"));   
+            $('.results').append(template);
         }
     }
+
+    //Create a modal then send a get request on click
+    var modal = $('#myModal');     
+    // var btn = $(".imageModal");
+    var span = $(".close");
+
+    $(".results").on("click", ".imageModal", function(e){
+        var recipeId = $(this).parent().attr("data-id");   
+            $.ajax({
+                url: "https://cors-anywhere.herokuapp.com/http://food2fork.com/api/get",
+                dataType: "json",
+                data: {
+                    key: "882f2168d0e0186c5f6ffdbd0e93b5e1",
+                    rId: recipeId
+                },
+            })
+            .done(function(data){
+                var allIngredients = data.recipe.ingredients;
+                // $('.modal-list').text("<p>" + allIngredients + "</p>");
+                organizeIngredients(allIngredients);
+                
+            });
+        modal.css({ 'display': "block" });
+    });
+
+    function organizeIngredients(ingredients){
+        for( i=0; i < ingredients.length; i++ ){
+            var item = ingredients[i];
+            $(".modal-list").append('<li>' + item + '</li>');
+        }
+    }
+
+    span.on("click", function(){
+        modal.css({ 'display': "none" });
         
+    });
+
+   
 });
