825ab6b9a086cd419c45a1386f06079498c9a117
Jezzica <jezzicamae@gmail.com>
2017-07-14 08:08:47 -0400

    reset results

--- js/script.js
+++ js/script.js
@@ -1,75 +1,14 @@
 $(document).ready(function(){
 
-$('.search').hide();
-$('.recipe-lists').hide();
-$('.btn-home').on("click" , function(){
-    $('.home').hide();
-    $('.search').fadeIn();
-    $('#go-home').on("click",function(){
-        $('.home').show();
-        $('.search').hide();
-    });
-    $('#go-search').on("click",function(){
-        $('.search').show();
-        $('.recipe-lists').hide();
-    });
+    var recipes = [];
+    var ingredientsList = [];
+        //Make inout values be gone on focus
+    var input = $('input.ingredients');
 
-});
-
-var recipes = [];
-var ingredientsList = [];
-    //Make inout values be gone on focus
-        var input = $('input.ingredients');
-
-
-    function addIngredientsToList(index){
-        $('input.ingredients').each(function(index){
-            // console.log($(this).val());
-            ingredientsList.push($(this).val())
-
-        });
-    }
-
-    //Add and remove input fields
-
-        var maxItems = 6;
-        var x = 1;
-        $('#plus').on("click", function(e){
-            e.preventDefault();
-            if(x < maxItems){
-                x++;
-            $(".add-input").append('<div><input class="ingredients" type="text"><i id="minus" class="fa fa-minus-circle" aria-hidden="true"></i><div>');
-            }
-            
-            });
-        $(".add-input").on("click", "#minus", function(e){ //user click on remove text
-            e.preventDefault(); 
-            $(this).parent('div').remove(); x--;
-        });
-
-
-  
-    $("form").on("submit", function(e){
-        e.preventDefault();
-        addIngredientsToList();
-        $('.search').hide();
-        $('.recipe-lists').fadeIn();
-        var ingredients = $('.ingredients').val();
-
-        $.ajax({
-            url: "https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search",
-            dataType: "json",
-            data: {
-                key: "882f2168d0e0186c5f6ffdbd0e93b5e1",
-                q: ingredientsList.join()
-            },
-        })
-        .done(function(data){
-            recipes = data.recipes;
-            listMyRecipe(recipes);
-        });
-
-    });
+    //Create a modal then send a get request on click
+    var modal = $('#myModal');     
+    // var btn = $(".imageModal");
+    var span = $(".close");
 
 
     //Loop through recipeList array and append results to html
@@ -88,10 +27,85 @@ var ingredientsList = [];
         }
     }
 
-    //Create a modal then send a get request on click
-    var modal = $('#myModal');     
-    // var btn = $(".imageModal");
-    var span = $(".close");
+    function addIngredientsToList(index){
+        $('input.ingredients').each(function(index){
+            // console.log($(this).val());
+            ingredientsList.push($(this).val())
+
+        });
+    }
+
+    function organizeIngredients(ingredients){
+        for( i=0; i < ingredients.length; i++ ){
+            var item = ingredients[i];
+            $(".modal-list").append('<li>' + item + '</li>');
+        }
+    }
+
+    $('.search').hide();
+    $('.recipe-lists').hide();
+    $('.btn-home').on("click" , function(){
+        $('.home').hide();
+        $('.search').fadeIn();
+        $('#go-home').on("click",function(){
+            $('.home').show();
+            $('.search').hide();
+        });
+        $('#go-search').on("click",function(){
+            $('.search').show();
+            $('.ingredients').val('');
+            $('.recipe-lists').hide();
+        });
+
+    });
+
+    //Add and remove input fields
+
+        var maxItems = 6;
+        var x = 1;
+        $('#plus').on("click", function(e){
+            e.preventDefault();
+            if(x < maxItems){
+                x++;
+            $(".add-input").append('<div><input class="ingredients" type="text"><i id="minus" class="fa fa-minus-circle" aria-hidden="true"></i><div>');
+            }
+            
+            });
+        $(".add-input").on("click", "#minus", function(e){ //user click on remove text
+            e.preventDefault(); 
+            $(this).parent('div').remove(); x--;
+        });
+  
+    $("form").on("submit", function(e){
+        e.preventDefault();
+        addIngredientsToList();
+        $('.search').hide();
+        $('.recipe-lists').fadeIn();
+        $('.loader').show();
+        var ingredients = $('.ingredients').val();
+
+        $.ajax({
+            url: "https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search",
+            dataType: "json",
+            data: {
+                key: "882f2168d0e0186c5f6ffdbd0e93b5e1",
+                q: ingredientsList.join()
+            },
+        })
+        .done(function(data){
+            recipes = data.recipes;
+            $('.loader').fadeOut(); 
+            $('.results').empty();
+            if( recipes.length == 0){
+                $('.results').append('results not found!');
+            }
+            // console.log(recipes);
+            listMyRecipe(recipes);
+            
+        });
+
+    });
+
 
     $(".results").on("click", ".imageModal", function(e){
         $(".modal-list").empty();
@@ -114,21 +128,8 @@ var ingredientsList = [];
             });
         modal.css({ 'display': "block" });
     });
-
-    function organizeIngredients(ingredients){
-        for( i=0; i < ingredients.length; i++ ){
-            var item = ingredients[i];
-            $(".modal-list").append('<li>' + item + '</li>');
-        }
-
-    }
-
     span.on("click", function(){
         modal.css({ 'display': "none" });
-
-        
-        
-    });
-
-   
+ 
+    });   
 });
