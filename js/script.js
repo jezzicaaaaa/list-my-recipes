792d2751b7d0e1ebcfec1493ecad7605e18c1afa
Jezzica <jezzicamae@gmail.com>
2017-07-15 15:09:46 -0400

    added see more button, designed results page

--- js/script.js
+++ js/script.js
@@ -10,6 +10,8 @@ $(document).ready(function(){
     // var btn = $(".imageModal");
     var span = $(".close");
 
+    var page = 1;
+
 
     //Loop through recipeList array and append results to html
     function listMyRecipe(recipeList){
@@ -17,13 +19,14 @@ $(document).ready(function(){
             var item = recipeList[i];  
             //Make a template to append to results
             var template = `
-                <div class="item" col-sm-6 col-md-6 data-id='${item.recipe_id}'>
-                    <img class="imageModal" src="${item.image_url}">
-                    <h3>${item.title}</h3>
+                <div class="item" data-id='${item.recipe_id}' style="background-image: url(${item.image_url});">
+                    <div class="title-container">
+                        <h3>${item.title}</h3>
+                    </div>
                 </div>
             `;            
             // console.log($('.item').data("id"));   
-            $('.row').append(template);
+            $('.results').append(template);
         }
     }
 
@@ -31,12 +34,11 @@ $(document).ready(function(){
         $('input.ingredients').each(function(index){
             // console.log($(this).val());
             ingredientsList.push($(this).val())
-
         });
     }
 
     function organizeIngredients(ingredients, image){
-        $(".image").append('<img src="' + image + '">');
+        $(".image").css('background-image', 'url(' + image + ')');
         for( i=0; i < ingredients.length; i++ ){
             var item = ingredients[i];
             $(".modal-list").append('<li>' + item + '</li>');
@@ -44,40 +46,46 @@ $(document).ready(function(){
            
         }
     }
+    
 
     $('.search').hide();
     $('.recipe-lists').hide();
     $('.btn-home').on("click" , function(){
         $('.home').hide();
         $('.search').fadeIn();
+        $('.ingredients').focus();
         $('#go-home').on("click",function(){
             $('.home').show();
             $('.search').hide();
         });
         $('#go-search').on("click",function(){
+            page = 1;
+            ingredientsList = [];
+            $('.add-input').empty();
             $('.search').show();
             $('.ingredients').val('');
             $('.recipe-lists').hide();
+            $('.see-more').hide();
         });
 
     });
 
     //Add and remove input fields
 
-        var maxItems = 6;
-        var x = 1;
-        $('#plus').on("click", function(e){
-            e.preventDefault();
-            if(x < maxItems){
-                x++;
-            $(".add-input").append('<div><input class="ingredients" type="text"><i id="minus" class="fa fa-minus-circle" aria-hidden="true"></i><div>');
-            }
-            
-            });
-        $(".add-input").on("click", "#minus", function(e){ //user click on remove text
-            e.preventDefault(); 
-            $(this).parent('div').remove(); x--;
+    var maxItems = 6;
+    var x = 1;
+    $('#plus').on("click", function(e){
+        e.preventDefault();
+        if(x < maxItems){
+            x++;
+        $(".add-input").append('<div><input class="ingredients" type="text"><i id="minus" class="fa fa-minus-circle" aria-hidden="true"></i><div>');}
+        $('.ingredients').focus();
+        
         });
+    $(".add-input").on("click", "#minus", function(e){ //user click on remove text
+        e.preventDefault(); 
+        $(this).parent('div').remove(); x--;
+    });
   
     $("form").on("submit", function(e){
         e.preventDefault();
@@ -98,23 +106,51 @@ $(document).ready(function(){
         .done(function(data){
             recipes = data.recipes;
             $('.loader').fadeOut(); 
-            $('.row').empty();
+            $('.results').empty();
             if( recipes.length == 0){
-                $('.row').append('<p class ="no-result">results not found!</p>');
+                $('.results').append('<p class ="no-result">results not found! :(</p>');
             }
             // console.log(recipes);
             listMyRecipe(recipes);
-            
+            if (data.count == 30){
+                $('.see-more').show();
+            }  
         });
+    });
+    $('.see-more').on('click', function(){
+        $(this).html('<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span>')
+        page++;
+        $.ajax({
+            url: "https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search",
+            dataType: "json",
+            data: {
+                key:  "882f2168d0e0186c5f6ffdbd0e93b5e1",
+                q: ingredientsList.join(),
+                page: page 
+            },
+        })
+        .done(function(data){
+            $('.see-more').html('see more');
+            recipes = data.recipes;
+            $('.loader').fadeOut(); 
+            // console.log(recipes);
+            listMyRecipe(recipes);
+            
+            if (data.count == 30){
+                $('.see-more').show();
+            }  
+            else $(".see-more").hide();
+        });
+        
 
     });
 
-
-    $(".results").on("click", ".imageModal", function(e){
+    $(".results").on("click", ".title-container", function(e){
         $(".modal-list").empty();
         $('.image').empty();
         $('.loader').show();
         var recipeId = $(this).parent().attr("data-id");   
+        console.log(recipeId); 
             $.ajax({
                 url: "https://cors-anywhere.herokuapp.com/http://food2fork.com/api/get",
                 dataType: "json",
@@ -127,14 +163,12 @@ $(document).ready(function(){
                 $('.loader').fadeOut();
                 var allIngredients = data.recipe.ingredients;
                 var image = data.recipe.image_url;
-                // $('.modal-list').text("<p>" + allIngredients + "</p>");
-                organizeIngredients(allIngredients, image);
-                
+                organizeIngredients(allIngredients, image);        
             });
         modal.css({ 'display': "block" });
     });
     span.on("click", function(){
         modal.css({ 'display': "none" });
- 
     });   
 });
+
